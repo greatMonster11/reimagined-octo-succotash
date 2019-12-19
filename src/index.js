ca95f5052eba790f74bc8d0d36105f628b84f90f
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2019-12-19 23:13:12 +0700

    add - Graphql with config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import AppRouter from "./routes";
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const client = new ApolloClient({
+  uri: "http://localhost:3000/grpahql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <AppRouter />
+  </ApolloProvider>,
+  document.getElementById("app")
+);
